607448cd5fb6da5abc28afbaef42c3dc036a10c6
Andres Munoz <ganonx9@gmail.com>
2023-12-26 18:05:02 -0500

    adding slider

--- script.js
+++ script.js
@@ -14,10 +14,29 @@ document.addEventListener("DOMContentLoaded", function() {
   
         // Scroll to the target section
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
+          behavior: 'smooth',block: "start"
+
         });
       });
     });
+    const cardMenu = document.querySelector('.card-menu');
+    const toggleButton = document.getElementById('toggleButton');
+
+    toggleButton.addEventListener('click', function() {
+        // Toggle the menu by updating its left position
+        const menuVisible = cardMenu.style.left === '0px';
+        cardMenu.style.left = menuVisible ? '-250px' : '0';
+    });
   });
   
-  
+  document.addEventListener('DOMContentLoaded', function() {
+    const cardsMenu = document.querySelector('.cards-menu');a
+    const toggleButton = document.getElementById('toggleButton');
+
+    toggleButton.addEventListener('click', function() {
+        // Toggle the menu by updating its transform property
+        const menuVisible = cardsMenu.style.transform === 'translateX(0%)';
+        cardsMenu.style.transform = menuVisible ? 'translateX(-100%)' : 'translateX(0%)';
+    });
+});
+
