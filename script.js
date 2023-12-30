5c0d3e27522ffcc68ff507a828fb1320b8cbdd44
Andres Munoz <ganonx9@gmail.com>
2023-12-29 21:35:36 -0500

    added first project and about me section

--- script.js
+++ script.js
@@ -19,24 +19,42 @@ document.addEventListener("DOMContentLoaded", function() {
         });
       });
     });
-    const cardMenu = document.querySelector('.card-menu');
-    const toggleButton = document.getElementById('toggleButton');
+  //   const cardMenu = document.querySelector('.card-menu');
+  //   const toggleButton = document.getElementById('toggleButton');
 
-    toggleButton.addEventListener('click', function() {
-        // Toggle the menu by updating its left position
-        const menuVisible = cardMenu.style.left === '0px';
-        cardMenu.style.left = menuVisible ? '-250px' : '0';
-    });
-  });
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const cardsMenu = document.querySelector('.cards-menu');a
-    const toggleButton = document.getElementById('toggleButton');
+  //   toggleButton.addEventListener('click', function() {
+  //       // Toggle the menu by updating its left position
+  //       const menuVisible = cardMenu.style.left === '0px';
+  //       cardMenu.style.left = menuVisible ? '-250px' : '0';
+  //   });
 
-    toggleButton.addEventListener('click', function() {
-        // Toggle the menu by updating its transform property
-        const menuVisible = cardsMenu.style.transform === 'translateX(0%)';
-        cardsMenu.style.transform = menuVisible ? 'translateX(-100%)' : 'translateX(0%)';
-    });
+  //   toggleButton.addEventListener('click', function() {
+  //     // Toggle the menu by updating its transform property
+  //     const menuVisible = cardsMenu.style.transform === 'translateX(0%)';
+  //     cardsMenu.style.transform = menuVisible ? 'translateX(-100%)' : 'translateX(0%)';
+  // });
+  var age= document.getElementById('age');
+  age.innerHTML=age.innerHTML.replace("X",getAge())
 });
+  
+//   document.addEventListener('DOMContentLoaded', function() {
+//     const cardsMenu = document.querySelector('.cards-menu');a
+//     const toggleButton = document.getElementById('toggleButton');
+
+//     toggleButton.addEventListener('click', function() {
+//         // Toggle the menu by updating its transform property
+//         const menuVisible = cardsMenu.style.transform === 'translateX(0%)';
+//         cardsMenu.style.transform = menuVisible ? 'translateX(-100%)' : 'translateX(0%)';
+//     });
+// });
+
+function getAge(){
+  var today = new Date();
+  var age  = today.getFullYear() - 2003;  
+  if (today.getMonth() <8 || today.getMonth() == 8 && today.getDate()<2)
+    age--;
+  return age;
+
+}
+
 
